057b9d8f7c4ee56e136cb367342960dab2475acf
quynhnam13 <joguerraclpun0suy@hotmail.com>
2024-05-11 02:44:51 +0700

    aptos commit

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-const randomNumber = (min, max) =>
-  Math.floor(Math.random() * (max - min + 1) + min);
+function removeNthFromEnd(head, n) {
+  const dummy = new ListNode(0);
+  dummy.next = head;
+  let first = dummy;
+  let second = dummy;
+  for (let i = 1; i <= n + 1; i++) {
+    first = first.next;
+  }
+  while (first !== null) {
+    first = first.next;
+    second = second.next;
+  }
+  second.next = second.next.next;
+  return dummy.next;
+}
