fc0ffc286aa34cd30441fc2c1f9006f5bfa39fbb
quynhnam13 <joguerraclpun0suy@hotmail.com>
2024-05-12 06:45:03 +0700

    demo project commit

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-function removeNthFromEnd(head, n) {
-  const dummy = new ListNode(0);
-  dummy.next = head;
-  let first = dummy;
-  let second = dummy;
-  for (let i = 1; i <= n + 1; i++) {
-    first = first.next;
-  }
-  while (first !== null) {
-    first = first.next;
-    second = second.next;
-  }
-  second.next = second.next.next;
-  return dummy.next;
-}
+const intersection = (a, b) => {
+  const s = new Set(b);
+  return a.filter((x) => s.has(x));
+};
