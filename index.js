298b2d151f11a9f776de64c94738400b13b8f18d
quynhnam13 <joguerraclpun0suy@hotmail.com>
2024-05-14 06:58:03 +0700

    aptos sdk

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-function countVowels(str) {
-  return str.match(/[aeiou]/gi).length;
+function findMin(nums) {
+  let left = 0;
+  let right = nums.length - 1;
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] > nums[right]) left = mid + 1;
+    else if (nums[mid] < nums[right]) right = mid;
+    else right--;
+  }
+  return nums[left];
 }
