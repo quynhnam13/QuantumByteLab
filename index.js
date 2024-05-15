b7a1c16f4d7e0324d384d5d358e10aef4aa036cb
quynhnam13 <joguerraclpun0suy@hotmail.com>
2024-05-16 02:41:17 +0700

    demo sdk

--- index.js
+++ index.js
@@ -1,33 +1,11 @@
-function totalNQueens(n) {
-  let count = 0;
-  const board = Array.from({ length: n }, () =>
-    Array.from({ length: n }, () => "."),
+function isValidBST(root) {
+  return isValid(root, null, null);
+}
+function isValid(node, min, max) {
+  if (!node) return true;
+  if ((min !== null && node.val <= min) || (max !== null && node.val >= max))
+    return false;
+  return (
+    isValid(node.left, min, node.val) && isValid(node.right, node.val, max)
   );
-  backtrack(0);
-  return count;
-  function backtrack(row) {
-    if (row === n) {
-      count++;
-      return;
-    }
-    for (let col = 0; col < n; col++) {
-      if (isValid(row, col)) {
-        board[row][col] = "Q";
-        backtrack(row + 1);
-        board[row][col] = ".";
-      }
-    }
-  }
-  function isValid(row, col) {
-    for (let i = 0; i < row; i++) {
-      if (board[i][col] === "Q") return false;
-    }
-    for (let i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
-      if (board[i][j] === "Q") return false;
-    }
-    for (let i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
-      if (board[i][j] === "Q") return false;
-    }
-    return true;
-  }
 }
